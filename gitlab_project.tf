906c58ab892c04a35ddc3d3df3318934c8b05c7d
159753852123 <73815175+159753852123@users.noreply.github.com>
2021-10-11 20:12:13 +0400

    Create gitlab_project.tf

---
+++ gitlab_project.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    gitlab = {
+      source = "gitlabhq/gitlab"
+      version = "3.7.0"
+    }
+  }
+}
+# Configure the GitLab Provider
+provider "gitlab" {
+    token = "jN5cz-syACyY3Phwf6Dz"
+}
+resource "gitlab_project" "terrafor1" {
+  name        = "terraform1"
+  description = "My terraform code"
+
+  visibility_level = "public"
+}
+
